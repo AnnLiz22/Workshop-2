7e19b929b98906e3a4665a6f68d9ca14df0da2d9
AnnLiz22 <bielakam@gmail.com>
2024-04-23 09:27:55 +0200

    user dao-crud pattern

---
+++ src/main/java/pl/coderslab/dao/UserDao.java
@@ -0,0 +1,36 @@
+package pl.coderslab.dao;
+
+public class UserDao {
+
+    //dodawanie użytkownika,
+    //zmiana danych,
+    //pobieranie po id,
+    //usuwanie po id,
+    //pobieranie wszystkich użytkowników.
+
+    private static final String QUERY_ADD_USER = "";
+    private static final String QUERY_ALTER_DATA = "";
+    private static final String QUERY_SELECT_BY_ID = "";
+    private static final String QUERY_DELETE_BY_ID = "";
+    private static final String QUERY_SELECT_ALL = "";
+
+
+    public static void createUser(){
+
+    }
+    public static void updateUser(){
+
+    }
+
+    public static void readUserById(){
+
+    }
+
+    public static void deleteUserById(){
+
+    }
+
+    public static void readAllUsers(){
+
+    }
+}
